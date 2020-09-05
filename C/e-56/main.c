498bfd05d543f665a1c0b5d0ed3c6d97513da51e
Todor Dimitrov <tosheto99.bg@abv.bg>
2020-09-05 21:44:43 +0300

    e-56 refactored

--- C/e-56/main.c
+++ C/e-56/main.c
@@ -14,16 +14,39 @@ if(argc != 3 ) {
 
 char *file = argv[1], *dest = argv[2];
 
-ssize_t fd1 = open(file, O_RDONLY);
+//ssize_t fd1 = open(file, O_RDONLY);
 ssize_t fd2 = open(dest, O_CREAT | O_TRUNC | O_RDWR, 0644);
-if(fd1 == -1 || fd2 == -1){
+if(fd2 == -1){
 	err(2, "Cant open file");
 }
 
+int a[2];
+if( pipe(a) == -1){
+	err(1, "cant pipe");
+}
+const pid_t child = fork();
+if( child == -1){
+	err(3, "cant fork");
+}
+
+if(child == 0){
+	close(a[0]);
+	if(dup2(a[1],1) == -1){
+		err(5, "cant dup2");
+	}
+	if(execlp("xxd", "xxd","-p", file, (char*)NULL) == -1){
+		err(4, "cant exec command");
+	}
+}
+wait(NULL);
+close(a[1]);
+if(dup2(a[0],0) == -1){
+	err(6, "cant dup2");
+}
 ssize_t reader;
 char buf;
 bool flag = 0;
-while((reader = read(fd1, &buf, sizeof(buf))) > 0) {
+while((reader = read(a[0], &buf, sizeof(buf))) > 0) {
 	if(buf == 0x55){
 		continue;
 	} else if(buf == 0x7D) {
